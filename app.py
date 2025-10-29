c456fa44eac08d84b1b0bfca514f6174438fc99d
Nancy Adelice Umutoniwase <nancytoniw@gmail.com>
2025-10-29 19:44:35 +0200

    Dynamic Routing

--- app.py
+++ app.py
@@ -5,8 +5,23 @@ app=Flask(__name__)
 
 # create the first route which is a homepage
 @app.route("/")
-def home():
-	return "Welcome to my Flask App"
+def index():
+	return "<h1>Python Operations with Flask Routing and Views</h1>"
+# create a second route (/print/Hello) with a string_print view
+
+@app.route("/print/<x>")
+def print_string(x):
+	print(x) # for the console 
+	return x # for the browser
+
+# Third route (/count/4)
+@app.route("/count/<int:num>")
+def count(num):
+	result=""
+	for i in range(1,num+1):
+		result+= str(i) + "<br>"
+	return result 
+@app.route("/math/<int:num1>/<operation>/<int:num2>")
 
 # Run the development server 
 if __name__=="__main__":
